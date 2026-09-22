212673ebfe04810288ffae559793a36816da8d51
Aishairshad-work <aishairshad09000@gmail.com>
2026-09-22 19:13:33 +0500

    Integrate WizzTech Protection

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from './App';
 import { CartProvider } from './context/CartContext';
 import { CurrencyProvider } from './context/CurrencyContext';
 import './styles/index.css';
+import { WizzTechProtectionProvider } from "@wizztech/protection";
+import "@wizztech/protection/dist/style.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CurrencyProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </CurrencyProvider>
+    <WizzTechProtectionProvider platformUrl={import.meta.env.VITE_WIZZTECH_PLATFORM_URL}>
+      <CurrencyProvider>
+        <CartProvider>
+            <App />
+        </CartProvider>
+      </CurrencyProvider>
+    </WizzTechProtectionProvider>
   </React.StrictMode>
 );
